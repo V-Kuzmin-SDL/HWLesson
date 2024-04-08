fa08fdc64bca3b943b698dd2bc01fa586a603a7f
V-Kuzmin-SDL <161224746+V-Kuzmin-SDL@users.noreply.github.com>
2024-04-08 21:31:58 +0300

    Save try

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+
+
+int m = 8;
+int n = 50;
+
+PrintNumbersInRange(m, n);
+
+
+void PrintNumbersInRange(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.Write(m);
+        PrintNumbersInRange(m + 1, n);
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+
+int[] array = new int[] { 1, 2, 3, 4, 5 };
+PrintArrayReverse(array, array.Length - 1);
+    
+
+void PrintArrayReverse(int[] array, int index)
+{
+    if (index < 0)
+    {
+        return;
+    }
+    else
+    {
+        Console.WriteLine(array[index]);
+        PrintArrayReverse(array, index - 1);
+    }
+}
+
